ef1fab33016ff446b332f4f1b8107eed0c36bd70
MedFadl <mohamedmedfadl@gmail.com>
2026-03-06 16:54:15 +0200

    Replace remote with local project

---
+++ src/main/java/com/medhat/Main.java
@@ -0,0 +1,59 @@
+package com.medhat;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int years;
+        final byte MONTHS = 12;
+        final byte PERCENTAGE = 100;
+
+        while (true) {
+            System.out.print("Principal: ");
+            principal = input.nextDouble();
+            if (principal < 1000 || principal > 100_000) {
+
+                System.out.println("Please enter a number between 1k to 1M!!");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            rate = input.nextDouble();
+            if (rate <= 0 || rate > 30) {
+                System.out.println("Please enter a number between 0 to 30");
+                continue;
+            }
+            rate = (rate/ PERCENTAGE) / MONTHS;
+            break;
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = input.nextInt();
+            if (years < 1 || years > 30) {
+                System.out.println("Please enter a number between 0 to 30");
+                continue;
+            }
+            years = years * MONTHS;
+            double formulaDep = Math.pow((1 + rate), years);
+
+            double result = principal * ((rate * (formulaDep))) / ((formulaDep - 1));
+
+            NumberFormat current = NumberFormat.getCurrencyInstance();
+            current.format(result);
+            System.out.println("Mortgage: " + current.format(result));
+            break;
+        }
+
+    }
+}
