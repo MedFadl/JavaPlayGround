361677d184abfa2e1a3cacf5862a100054bbc3fa
MedFadl <mohamedmedfadl@gmail.com>
2026-03-08 01:26:44 +0200

    Trying oop :D

--- src/main/java/com/medhat/Main.java
+++ src/main/java/com/medhat/Main.java
@@ -1,102 +1,11 @@
 package com.medhat;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
-    final static byte MONTHS_IN_YEAR = 12;
-    final static byte PERCENTAGE = 100;
-    static NumberFormat current = NumberFormat.getCurrencyInstance();
-    static Scanner input = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-
-        double principal = readValue("Principal: ",1000,1_000_000);
-        double rate = readValue("Annual Interest Rate: ",0,30);
-        int years = (int) readValue("Period (Years): ",0,30);
-
-
-
-        mortgageDetails(principal,rate,years);
-        input.close();
-
+        var mortgage = new Mortgage();
     }
 
 
-
-
-
-
-
-
-
-
-
-    /**
-     * This is a function takes number from user and validate it as if it is between 2 specific numbers or not
-     * @param prompt as a message to display before user enter a number
-     * @param min as minimum to put
-     * @param max as maximum to put
-     * @return returns a double value when it is validated! :'D
-     */
-
-    static double readValue(String prompt , double min , double max)
-    {
-        double value;
-        while (true) {
-            System.out.print(prompt);
-            value = input.nextDouble();
-            if (value >= min && value <= max) {
-                break;
-            }
-            System.out.println("Enter a value between " + min + " and " + max);
-        }
-        return value;
-    }
-
-    /**
-     * I love java docs :D
-     * @param principal takes principal value
-     * @param annualInterestRate takes annual rate
-     * @param months takes how many months should be calculated
-     * @return monthly payment based on the 3 input and Mortgage
-     */
-    static double monthlyPayment(double principal , double annualInterestRate, int months) {
-        double compoundFactor = Math.pow((1 + annualInterestRate), months);
-        return principal * (annualInterestRate * (compoundFactor)) / (compoundFactor - 1);
-    }
-
-
-    /**
-     * Created by medhat <b><3</b><br>
-     * provides detailed mortgage information "How much payment per month till it ends"
-     * @param principal takes original principal value
-     * @param rate takes the rate and converts it into annul rate
-     * @param years takes years and calculate it into months
-     *
-     */
-
-
-    static void mortgageDetails(double principal , double rate, int years){
-
-        double annualInterestRate = (rate / PERCENTAGE) / MONTHS_IN_YEAR;
-        short months = (short) (years * MONTHS_IN_YEAR);
-        double payment;
-
-
-        System.out.println("\n");
-        System.out.println("MORTGAGE");
-        System.out.println("------");
-        System.out.println(current.format(monthlyPayment(principal,annualInterestRate,months)));
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("------");
-        System.out.println("\n");
-
-        for (int i = 1; i <= months; i++)
-        {
-            double powed = Math.pow((1 + annualInterestRate), months);
-            payment = principal * (powed - Math.pow((1+annualInterestRate),i)) / (powed - 1);
-            System.out.println(current.format(payment));
-        }
-    }
 }
---
+++ src/main/java/com/medhat/Mortgage.java
@@ -0,0 +1,104 @@
+package com.medhat;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Mortgage {
+
+    // Some variables and instances <3
+    private static final byte MONTHS_IN_YEAR = 12;
+    private static final byte PERCENTAGE = 100;
+    private final NumberFormat current = NumberFormat.getCurrencyInstance();
+    private final Scanner input = new Scanner(System.in);
+
+    //Constructor
+    public  Mortgage(){
+        double principal = setValue("Principal: ", 1000, 1_000_000);
+        double rate = setValue("Annual Interest Rate: ", 0, 30);
+        int years = (int) setValue("Period (Years): ", 0, 30);
+        mortgageDetails(principal, rate, years);
+        input.close();
+    }
+
+
+
+    /**
+     * This is a function takes number from user and validate it as if it is between 2 specific numbers or not
+     * @param prompt as a message to display before user enter a number
+     * @param min as minimum to put
+     * @param max as maximum to put
+     * @return double value - returns a double value when it is validated! :'D
+     */
+
+    double setValue(String prompt , double min , double max)
+    {
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = input.nextDouble();
+            if (value >= min && value <= max) {
+                break;
+            }
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    /**
+     * I love java docs :D
+     * @param principal takes principal value
+     * @param annualInterestRate takes annual rate
+     * @param months takes how many months should be calculated
+     * @return double value - monthly payment based on the 3 input and Mortgage
+     */
+    private double monthlyPayment(double principal , double annualInterestRate, int months) {
+        if (annualInterestRate == 0) {
+            return principal / months;
+        }
+        double compoundFactor = Math.pow((1 + annualInterestRate), months);
+        return principal * (annualInterestRate * (compoundFactor)) / (compoundFactor - 1);
+    }
+
+
+    /**
+     * Created by medhat <b><3</b><br>
+     * provides detailed mortgage information "How much payment per month till it ends"
+     * @param principal takes original principal value
+     * @param rate takes the rate and converts it into annul rate
+     * @param years takes years and calculate it into months
+     *
+     */
+
+
+    private void mortgageDetails(double principal , double rate, int years){
+        double annualInterestRate = (rate / PERCENTAGE) / MONTHS_IN_YEAR;
+        short months = (short) (years * MONTHS_IN_YEAR);
+        double payment = 0;
+        System.out.println("\n");
+        System.out.println("MORTGAGE");
+        System.out.println("------");
+
+        if(annualInterestRate != 0) {
+            System.out.println(current.format(monthlyPayment(principal, annualInterestRate, months)));
+            System.out.println("PAYMENT SCHEDULE");
+            System.out.println("------");
+            System.out.println("\n");
+
+            for (int i = 1; i <= months; i++) {
+                double powed = Math.pow((1 + annualInterestRate), months);
+                payment = principal * (powed - Math.pow((1 + annualInterestRate), i)) / (powed - 1);
+                System.out.println(current.format(payment));
+            }
+        }
+            else {
+            System.out.println("PAYMENT SCHEDULE");
+            System.out.println("------");
+            System.out.println("\n");
+            for (int i = 1; i <= months; i++) {
+                System.out.println(current.format(principal / months));
+            }
+            }
+
+        }
+    }
+}
