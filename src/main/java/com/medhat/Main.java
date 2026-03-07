1f4ab40c85c41e3cb10c1c4c449fa1fdef5df56f
MedFadl <mohamedmedfadl@gmail.com>
2026-03-07 03:21:51 +0200

    more details and finalizing

--- src/main/java/com/medhat/Main.java
+++ src/main/java/com/medhat/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENTAGE = 100;
+    static NumberFormat current = NumberFormat.getCurrencyInstance();
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -15,19 +17,10 @@ public class Main {
 
 
 
-        NumberFormat current = NumberFormat.getCurrencyInstance();
-        System.out.println("Mortgage: " + current.format(monthlyPayment(principal,rate,years)));
+        mortgageDetails(principal,rate,years);
+        input.close();
 
     }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -50,7 +43,6 @@ public class Main {
     static double readValue(String prompt , double min , double max)
     {
         double value;
-        Scanner input = new Scanner(System.in);
         while (true) {
             System.out.print(prompt);
             value = input.nextDouble();
@@ -59,23 +51,52 @@ public class Main {
             }
             System.out.println("Enter a value between " + min + " and " + max);
         }
-        input.close();
         return value;
     }
 
     /**
      * I love java docs :D
      * @param principal takes principal value
-     * @param rate takes annual rate
-     * @param years takes how many years should be calculated
+     * @param annualInterestRate takes annual rate
+     * @param months takes how many months should be calculated
      * @return monthly payment based on the 3 input and Mortgage
      */
-    static double monthlyPayment(double principal , double rate, int years) {
-        double annualInterestRate = (rate / PERCENTAGE) / MONTHS_IN_YEAR;
-        years = years * MONTHS_IN_YEAR;
-        double compoundFactor = Math.pow((1 + annualInterestRate), years);
+    static double monthlyPayment(double principal , double annualInterestRate, int months) {
+        double compoundFactor = Math.pow((1 + annualInterestRate), months);
         return principal * (annualInterestRate * (compoundFactor)) / (compoundFactor - 1);
+    }
 
 
+    /**
+     * Created by medhat <b><3</b><br>
+     * provides detailed mortgage information "How much payment per month till it ends"
+     * @param principal takes original principal value
+     * @param rate takes the rate and converts it into annul rate
+     * @param years takes years and calculate it into months
+     *
+     */
+
+
+    static void mortgageDetails(double principal , double rate, int years){
+
+        double annualInterestRate = (rate / PERCENTAGE) / MONTHS_IN_YEAR;
+        short months = (short) (years * MONTHS_IN_YEAR);
+        double payment;
+
+
+        System.out.println("\n");
+        System.out.println("MORTGAGE");
+        System.out.println("------");
+        System.out.println(current.format(monthlyPayment(principal,annualInterestRate,months)));
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("------");
+        System.out.println("\n");
+
+        for (int i = 1; i <= months; i++)
+        {
+            double powed = Math.pow((1 + annualInterestRate), months);
+            payment = principal * (powed - Math.pow((1+annualInterestRate),i)) / (powed - 1);
+            System.out.println(current.format(payment));
+        }
     }
 }
