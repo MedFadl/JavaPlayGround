8deb54962a7385de284f8cdb6ae956e342d429c5
MedFadl <mohamedmedfadl@gmail.com>
2026-05-06 14:44:50 +0300

    SYNCHRONIZATION AND OBJECTS AS A LOCK PFFT

--- src/main/java/com/medhat/concurrency/DownloadStatus.java
+++ src/main/java/com/medhat/concurrency/DownloadStatus.java
@@ -5,22 +5,27 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DownloadStatus {
+    //Atomic integer is more thread-Safekeeping
     private final AtomicInteger totalBytes = new AtomicInteger();
+    private final Object totalBytesLock = new Object();
+    private final Object totalFilesLock = new Object();
 
-    //Syncronization with lock and unlock but  it is not the best practice so i commented it to learn :D
+    //Synchronization with lock and unlock, but it is not the best practice, so I commented it to learn :D
     //private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes.get();
     }
 
-    public void incrementTotalBytes() {
+    public void incrementTotalBytes() { //can add synchronized after public, but that's more bad
         //lock.lock();
-        try{
-            totalBytes.incrementAndGet();
-        }
-        finally {
+        //Another way to sync without a lock!!! with Synchronized () {} but don't add this as it will cause performance issue (unnecessary wait)
+                totalBytes.incrementAndGet();
+
+       // finally {
             // lock.unlock();
-        }
+       // }
     }
 }
+
+// A synchronization can add a deadlock! so take care please :D love you all <3
