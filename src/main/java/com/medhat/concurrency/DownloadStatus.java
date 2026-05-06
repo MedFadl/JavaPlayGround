7d59d8a683a6ae074993764c3e9314ad172e062e
MedFadl <mohamedmedfadl@gmail.com>
2026-05-06 14:34:56 +0300

    Syncronization (NOT RECOMMENDED)

--- src/main/java/com/medhat/concurrency/DownloadStatus.java
+++ src/main/java/com/medhat/concurrency/DownloadStatus.java
@@ -1,15 +1,26 @@
 package com.medhat.concurrency;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DownloadStatus {
     private final AtomicInteger totalBytes = new AtomicInteger();
+
+    //Syncronization with lock and unlock but  it is not the best practice so i commented it to learn :D
+    //private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes.get();
     }
 
     public void incrementTotalBytes() {
-        totalBytes.incrementAndGet();
+        //lock.lock();
+        try{
+            totalBytes.incrementAndGet();
+        }
+        finally {
+            // lock.unlock();
+        }
     }
 }
