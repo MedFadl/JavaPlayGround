cebc8bd4e354c2d118c3552799ad25174c3fd513
MedFadl <mohamedmedfadl@gmail.com>
2026-05-10 12:36:15 +0300

    Callables

--- src/main/java/com/medhat/ExcutiveFramework/ExecutorsDemo.java
+++ src/main/java/com/medhat/ExcutiveFramework/ExecutorsDemo.java
@@ -1,5 +1,6 @@
 package com.medhat.ExcutiveFramework;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -10,9 +11,18 @@ public class ExecutorsDemo {
         try {
             exec = Executors.newFixedThreadPool(2);
             for (int i = 0; i < 10; i++) {
-                exec.submit(() -> {
+                var future = exec.submit(() -> {
+                    LongTask.simulate();
                     System.out.println(Thread.currentThread().getName());
+                    return 1;
                 });
+                System.out.println("bla bla bla");
+                try {
+                    var result = future.get();
+                    System.out.println(result);
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
             }
         } finally {
             assert exec != null;
---
+++ src/main/java/com/medhat/ExcutiveFramework/LongTask.java
@@ -0,0 +1,12 @@
+package com.medhat.ExcutiveFramework;
+
+public class LongTask {
+    public static void simulate()
+    {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
